d57a9bddb2a1a9618fcc2c1757e283f204c5db89
Denis <denis-shkurko@yandex.ru>
2022-10-04 14:00:11 +0700

    задача №2

--- Seminar_3.py
+++ Seminar_3.py
@@ -1,4 +1,5 @@
 import os
+from traceback import print_list
 from unicodedata import name
 os.system("cls")
 
@@ -8,11 +9,28 @@ os.system("cls")
 # позициях(не индексах).
 
 import random
+# num=int(input('Введите число '))
+# num_list=[]
+# for i in range(num):
+#    num_list.append(random.randint(1,10))
+# print(num_list)
+# print(sum(num_list[::2]))  #срез, складываем элементы через один начиная с первого.
+
+###############################################################
+
+# №2. Напишите программу, которая найдёт произведение
+# пар чисел списка. Парой считаем первый и последний
+# элемент, второй и предпоследний и т.д.
+# import random
 num=int(input('Введите число '))
 num_list=[]
 for i in range(num):
    num_list.append(random.randint(1,10))
 print(num_list)
-print(sum(num_list[::2]))  #складывае элементы через один начиная с первого.
+my_list=[]
+for i in range((len(num_list)+1)//2):
+   my_list.append(num_list[i]*num_list[-1-i])
+print(my_list)
 
-###############################################################
+
+
